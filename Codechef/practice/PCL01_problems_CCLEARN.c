4ba3feae9eca2e26253628867701921e5c63a526
SayanDasModak <sayandasmodak98@gmail.com>
2024-03-18 20:23:28 +0530

    problem_CCLEARN

---
+++ Codechef/practice/PCL01_problems_CCLEARN.c
@@ -0,0 +1,10 @@
+// CodeChef Learn Problem Solving In the new CodeChef Learn module, under the "Learn Problem Solving" section, there are two courses for each language. For eg. "Python Beginner - Part 1" and "Python Beginner - Part 2". These courses help you get started with CodeChef contests. Currently there are courses for 4 languages, and hence there are 8 courses in this section. But suppose there are courses for N languages, what will be the total number of courses in this section?
+
+#include<stdio.h>
+void main(){
+	int x,y;
+	scanf("%d",&x);
+	y = x * 2;
+	printf("%d",y);
+	
+}
