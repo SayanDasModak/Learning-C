3bfa61b13faaacf25a0451f4653be6e04500f1d7
SayanDasModak <sayandasmodak98@gmail.com>
2024-03-18 23:37:24 +0530

    problem_GDTURN

---
+++ Codechef/practice/PCL05_problems_GDTURN.c
@@ -0,0 +1,16 @@
+// Good Turn Chef and Chefina are playing with dice. In one turn, both of them roll their dice at once.They consider a turn to be good if the sum of the numbers on their dice is greater than  6 6. Given that in a particular turn Chef and Chefina got X andY on their respective dice, find whether the turn was good.
+#include <stdio.h>
+int main(){
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x,y;
+	    scanf("%d%d",&x,&y);
+	    if (x + y > 6) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+	return 0;
+}
